e98c1b061236f710f7ba151394fd2c6e4b5a94f0
mohammadnejati <mnejati766@gmail.com>
2024-12-15 22:17:28 +0330

    finall

---
+++ src/components/Nejati.tsx
@@ -0,0 +1,15 @@
+import "./Nejati.scss";
+
+function Nejati() {
+  return (
+    <>
+      <div className="nejati">
+      
+          <p>Nejati Quiz Game</p>
+   
+      </div>
+    </>
+  );
+}
+
+export default Nejati;
